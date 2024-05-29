594c6ca3e73e921f49032e3bd80344094a3057cc
MuhamadRifkii <mrifkibhae@gmail.com>
2024-05-29 07:53:34 +0700

    Refactor launcher activity to be MainActivity instead

--- app/src/main/java/com/dicoding/aquaculture/view/MainActivity.kt
+++ app/src/main/java/com/dicoding/aquaculture/view/MainActivity.kt
@@ -3,13 +3,8 @@ package com.dicoding.aquaculture.view
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.dicoding.aquaculture.R
 import com.dicoding.aquaculture.databinding.ActivityMainBinding
-import com.dicoding.aquaculture.databinding.ActivityWelcomeBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
