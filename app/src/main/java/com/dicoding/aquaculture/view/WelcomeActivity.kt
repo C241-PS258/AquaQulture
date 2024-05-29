66c4504983199c6b4aa302bb37124c28a9c6faf7
MuhamadRifkii <mrifkibhae@gmail.com>
2024-05-29 07:28:16 +0700

    Add sharedpreference, so the welcome screen only displayed once

--- app/src/main/java/com/dicoding/aquaculture/view/WelcomeActivity.kt
+++ app/src/main/java/com/dicoding/aquaculture/view/WelcomeActivity.kt
@@ -1,15 +1,9 @@
 package com.dicoding.aquaculture.view
 
+import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.view.WindowInsets
-import android.view.WindowManager
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.dicoding.aquaculture.R
 import com.dicoding.aquaculture.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
@@ -17,16 +11,32 @@ class WelcomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityWelcomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        binding.button.setOnClickListener {
-            intent = Intent(this, MainActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-            startActivity(intent)
-            finish()
+        val sharedPref = getSharedPreferences("app_preferences", Context.MODE_PRIVATE)
+        val hasShownWelcome = sharedPref.getBoolean("hasShownWelcome", false)
+
+        if (hasShownWelcome) {
+            startMainActivity()
+        } else {
+            binding = ActivityWelcomeBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+            binding.button.setOnClickListener {
+                with(sharedPref.edit()) {
+                    putBoolean("hasShownWelcome", true)
+                    apply()
+                }
+                startMainActivity()
+            }
+            setupView()
         }
-        setupView()
+    }
+
+    private fun startMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+        finish()
     }
 
     private fun setupView() {
