0a0c759aaab0b7685c0132c838da81560670e832
MuhamadRifkii <mrifkibhae@gmail.com>
2024-05-29 07:18:22 +0700

    Add WelcomeActivity with MotionLayout

---
+++ app/src/main/java/com/dicoding/aquaculture/view/WelcomeActivity.kt
@@ -0,0 +1,35 @@
+package com.dicoding.aquaculture.view
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dicoding.aquaculture.R
+import com.dicoding.aquaculture.databinding.ActivityWelcomeBinding
+
+class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish()
+        }
+        setupView()
+    }
+
+    private fun setupView() {
+        supportActionBar?.hide()
+    }
+}
